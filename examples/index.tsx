76631f24e5556038217591c1953054aefe652c6b
Paddy Mullen <paddy@paddymullen.com>
2024-10-24 10:27:35 -0400

    major packages updated,  npm run build works

--- examples/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-// eslint-disable-next-line no-console
-console.debug('React 16/17 mode');
-ReactDOM.render(<App />, document.getElementById('root'));
